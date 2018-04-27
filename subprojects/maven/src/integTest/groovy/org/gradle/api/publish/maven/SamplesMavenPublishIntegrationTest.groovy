01d1194bb8eb4cd50e4a5fabf8ce88e826a7d4da
Marc Philipp <marc@gradle.com>
2018-04-27 16:19:10 +0200

    Remove model block from "Generating POM without publishing" sample
    The model block is no longer needed because publishing no longer relies
    on DeferredConfigurable. This commit removes it from the sample and
    deletes the note explaining why it was needed.

--- subprojects/maven/src/integTest/groovy/org/gradle/api/publish/maven/SamplesMavenPublishIntegrationTest.groovy
+++ subprojects/maven/src/integTest/groovy/org/gradle/api/publish/maven/SamplesMavenPublishIntegrationTest.groovy
@@ -205,6 +205,20 @@ class SamplesMavenPublishIntegrationTest extends AbstractIntegrationSpec {
         module.parsedPom.scm.url.text() == 'http://example.com/my-library/'
     }
 
+    @UsesSample("maven-publish/pomGeneration")
+    def pomGeneration() {
+        given:
+        sample sampleProject
+
+        when:
+        succeeds "generatePomFileForMavenCustomPublication"
+
+        then:
+        def pom = sampleProject.dir.file("build/generated-pom.xml").assertExists()
+        def parsedPom = new org.gradle.test.fixtures.maven.MavenPom(pom)
+        parsedPom.name == "Example"
+    }
+
     private void verifyPomFile(MavenFileModule module, String outputFileName) {
         def actualPomXmlText = module.pomFile.text.replaceFirst('publication="\\d+"', 'publication="«PUBLICATION-TIME-STAMP»"').trim()
         assert actualPomXmlText == getExpectedPomOutput(sampleProject.dir.file(outputFileName))
